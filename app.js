f0b33fb795d34bc4ba110717c37a669666c3578d
liangchow <liangchern@gmail.com>
2025-06-14 13:06:11 -0700

    Update app.js
    Add header to api request

--- app.js
+++ app.js
@@ -1,7 +1,10 @@
 const loadJoke = async() => {
     try {
-        const response = await fetch('https://api.chucknorris.io/jokes/random'
-        )
+        const response = await fetch('https://api.chucknorris.io/jokes/random',{
+            headers: {
+                Accept: "application/json"
+            }
+        })
 
         const json = await response.json()
         document.getElementById("loadingJoke").innerHTML = json.value

af92130235096cd640a5880cace98bf3117e5615
liangchow <liangchern@gmail.com>
2025-06-14 12:37:38 -0700

    Add chuck norris api
    app.js

---
+++ app.js
@@ -0,0 +1,13 @@
+const loadJoke = async() => {
+    try {
+        const response = await fetch('https://api.chucknorris.io/jokes/random'
+        )
+
+        const json = await response.json()
+        document.getElementById("loadingJoke").innerHTML = json.value
+    } catch(err){
+        console.log(err)
+    }
+}
+
+document.getElementById("loadJokeButton").addEventListener("click", loadJoke)

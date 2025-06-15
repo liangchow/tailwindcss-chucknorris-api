b7cef714dc86b402a4b4eb6480ce8f188a0eb59f
liangchow <liangchern@gmail.com>
2025-06-14 21:32:30 -0700

    type=module

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,18 @@
 import { defineConfig } from 'vite'
 import tailwindcss from '@tailwindcss/vite'
+
 export default defineConfig({
   plugins: [
     tailwindcss(),
   ],
-})
+  build: {
+    rollupOptions: {
+      input: 'index.html', // This is needed if using index.html as entry
+      output: {
+        entryFileNames: 'app.js', // Outputs main JS file as app.js
+      }
+    },
+    outDir: 'dist', // Default is 'dist', you can change this
+    emptyOutDir: true,
+  }
+});

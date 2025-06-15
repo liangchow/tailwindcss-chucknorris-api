d23b6812470cfb3d085e7fbad88a4dc56eda19df
liangchow <liangchern@gmail.com>
2025-06-14 21:06:57 -0700

    Update app.js

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
-const loadJoke = async(req, res) => {
+// const loadJoke = async(req, res) => {
+async function loadJoke(req, res) {
     try {
         const res = await fetch('https://api.chucknorris.io/jokes/random',{
             headers: {
@@ -9,6 +10,7 @@ const loadJoke = async(req, res) => {
         const json = await res.json()
         console.log(json)
         document.getElementById("loadingJoke").innerHTML = json.value
+
     } catch(err){
         console.log(err)
     }
